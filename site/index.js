de8000050f01031bbff13b9e909deac0bd236b37
David Anson <david@dlaa.me>
2026-01-14 19:14:17 -0800

    Simple fetch.

--- site/index.js
+++ site/index.js
@@ -1,6 +1,14 @@
 function onCheck() {
-  const result = "[result]";
-  document.getElementById("output").textContent = result;
+  const productUrl = new URL("TODO.htm", document.location);
+  fetch(productUrl).
+    then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP ${response.status}: ${response.statusText}`);
+      }
+      return response.text();
+    }).
+    catch((error) => error.message || error).
+    then((result) => document.getElementById("output").textContent = result)
 }
 
 document.getElementById("check").onclick = onCheck;
