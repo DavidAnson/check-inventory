623e4b64d4347456293dfc437bf8d7e525fbc37f
David Anson <david@dlaa.me>
2026-01-18 17:41:35 -0800

    Check category.

--- docs/index.js
+++ docs/index.js
@@ -2,17 +2,20 @@ const vendor = {
   productSkuSelector: "div.product-number span",
   productNameSelector: "h1.product-name",
   productColorSelector: "div.color-attribute span.selected-value",
-  productVariantSelector: "div.color-attribute li.variation-value:not(.selected) span.swatchanchor",
+  productVariantSelector: "div.color-attribute li.variation-value:not(.selected, .unselectable) span.swatchanchor",
   productInStockSelector: "div.size-attribute li.selectable span.swatchanchor-text",
   productSoldOutSelector: "div.size-attribute li.unselectable span.swatchanchor-text",
+  categoryProductSelector: "div.search-result-content a.name-link",
 };
 
 const checkElement = document.getElementById("check");
 const outputElement = document.getElementById("output");
 const progressElement = document.getElementById("progress");
 const urlElement = document.getElementById("url");
+const domParser = new DOMParser();
 
 function setOutput(message) {
+  console.log(message);
   outputElement.textContent = message;
 }
 
@@ -22,7 +25,10 @@ function setProgress(value) {
 
 function checkProductPage(productUrls, index, resultLines) {
   const { productUrl, isVariant } = productUrls[index];
-  return fetch(new URL(`https://cors-header-proxy.dlaa.workers.dev/corsproxy/?apiurl=${productUrl}`)).
+  setOutput(`${productUrl}...`);
+  const proxiedProductUrl = new URL("https://cors-header-proxy.dlaa.workers.dev/corsproxy/");
+  proxiedProductUrl.searchParams.append("apiurl", productUrl);
+  return fetch(proxiedProductUrl).
     then((response) => {
       if (!response.ok) {
         throw new Error(`HTTP ${response.status}: ${response.statusText}`);
@@ -30,24 +36,30 @@ function checkProductPage(productUrls, index, resultLines) {
       return response.text();
     }).
     then((responseText) => {
-      const domParser = new DOMParser();
       const productDocument = domParser.parseFromString(responseText, "text/html");
-      const productSKU = productDocument.querySelector(vendor.productSkuSelector).textContent.trim();
-      const productName = productDocument.querySelector(vendor.productNameSelector).textContent.trim();
-      const productColor = productDocument.querySelector(vendor.productColorSelector).textContent.trim();
-      const queries = [
-        [ "in-stock", vendor.productInStockSelector ],
-        [ "sold-out", vendor.productSoldOutSelector ]
-      ]
-      for (const [ availability, selector ] of queries) {
-        for (const sizeElement of productDocument.querySelectorAll(selector)) {
-          const productSize = sizeElement.textContent.trim();
-          resultLines.push(`"${productSKU}","${productName}","${productColor}","${productSize}","${availability}"`);
+      const categoryProducts = productDocument.querySelectorAll(vendor.categoryProductSelector);
+      if (categoryProducts.length > 0) {
+        for (const categoryProduct of categoryProducts) {
+          productUrls.push({ "productUrl": new URL(categoryProduct.dataset.href, productUrl), "isVariant": false });
         }
-      }
-      if (!isVariant) {
-        for (const productVariant of productDocument.querySelectorAll(vendor.productVariantSelector)) {
-          productUrls.push({ "productUrl": new URL(productVariant.dataset.swatchProductUrl), "isVariant": true });
+      } else {
+        const productSKU = productDocument.querySelector(vendor.productSkuSelector).textContent.trim();
+        const productName = productDocument.querySelector(vendor.productNameSelector).textContent.trim();
+        const productColor = productDocument.querySelector(vendor.productColorSelector).textContent.trim();
+        const queries = [
+          [ "in-stock", vendor.productInStockSelector ],
+          [ "sold-out", vendor.productSoldOutSelector ]
+        ]
+        for (const [ availability, selector ] of queries) {
+          for (const sizeElement of productDocument.querySelectorAll(selector)) {
+            const productSize = sizeElement.textContent.trim();
+            resultLines.push(`"${productSKU}","${productName}","${productColor}","${productSize}","${availability}"`);
+          }
+        }
+        if (!isVariant) {
+          for (const productVariant of productDocument.querySelectorAll(vendor.productVariantSelector)) {
+            productUrls.push({ "productUrl": new URL(productVariant.dataset.swatchProductUrl, productUrl), "isVariant": true });
+          }
         }
       }
     });
